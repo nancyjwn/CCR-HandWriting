31aded58f9bb2509cff76903deee1979695c7cba
Nancy Jiwono <nancyjiwono@gmail.com>
2026-09-14 09:42:07 +0800

    config change

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 config = {
-    'exp_name': '【0605】YOUR_EXPERIMENT_NAME',
-    'epoch': 500,
+    'exp_name': '【0001】FULL DATASET TRAIN',
+    'epoch': 100,
     'lr': 1.0,
-    'mode': 'stroke',  # character / stroke
-    'batch': 32,
-    'val_frequency': 1000,
+    'mode': 'stroke',
+    'batch': 64,
+    'val_frequency': 2000,
     'test_only': False,
-    'resume': '',
+    'resume': '',                      # KOSONG - karena training dari awal
     'train_dataset': './data/mydata/train_1000',
     'test_dataset': './data/mydata/test_1000',
     'weight_decay': False,
-    'schedule_frequency': 1000000,
+    'schedule_frequency': 5000,
     'image_size': 32,
     'alphabet': 3755,
 }
